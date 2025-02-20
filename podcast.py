a0371dbcd01d29689c4b104c486681aeb7105f7e
wctang <wctang@gmail.com>
2025-02-20 08:44:25 +0800

    update.

--- podcast.py
+++ podcast.py
@@ -32,8 +32,8 @@ def main():
     with open("index.html", "w", encoding="utf-8", newline='\n') as out:
         print(f'<!DOCTYPE html><html><head><title>{name}</title></head><body><h1>{name}</h1><img src="{base}/logo.png" /><a href="{base}/feed.xml">feed</a><ul>', file=out)
         for idx, item in enumerate(items):
-            print(f'<li><a href="{base}/{item[1]}">{name}</a></li>', file=out)
-        print(f'</ul></body><p>{_now.isoformat(timespec="seconds")}</p></html>', file=out)
+            print(f'<li><a href="{base}/{item[1]}">{item[0]}</a></li>', file=out)
+        print(f'</ul></body><p>{_now}</p></html>', file=out)
 
 
 if __name__ == '__main__':
